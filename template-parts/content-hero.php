be97fe6dc9d66c8b58d895d4f7fb4ce0988cebf4
Radu <Radu@Radu15s-MacBook-Pro.local>
2017-05-23 18:46:15 +0300

    Fixed the links for the archive fixed #8

--- template-parts/content-hero.php
+++ template-parts/content-hero.php
@@ -14,12 +14,11 @@
 		<div class="c-hero__background">
 
 			<?php if ( has_post_thumbnail() ) :
-				$thumb_id  = get_post_thumbnail_id();
+				$thumb_id = get_post_thumbnail_id();
 				$thumb_url = wp_get_attachment_image_src( $thumb_id, 'thumbnail-size', true );
 				?>
 
 				<img class="c-hero__image" src="<?php echo $thumb_url[0] ?>">
-
 
 			<?php else: ?>
 
@@ -29,33 +28,31 @@
 
 		</div>
 		<div class="c-hero__wrap">
+
 			<?php
-			the_title( '<h1 class="c-hero__entry-title">', '</h1>' );
 			if ( 'post' === get_post_type() ) : ?>
+
 				<div class="c-hero__entry-meta">
+
 					<?php begonia_lite_posted_on(); ?>
+
 				</div><!-- .entry-meta -->
+
 				<?php
 			endif; ?>
 
+			<a href="<?php the_permalink(); ?>">
+
+				<?php the_title( '<h1 class="c-hero__entry-title">', '</h1>' ); ?>
+
+			</a>
+
 			<div class="c-hero__content entry-content">
-				<?php
-				global $post;
-				// Check the content for the more text
-				$has_more = strpos( $post->post_content, '<!--more' );
-				if ( $has_more ) {
-					the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'begonia-lite' ) );
-				} else {
-					the_excerpt(); ?>
-					<div class="c-hero_read_more">
-						<a href="<?php the_permalink(); ?>"><?php _e( 'Read More', 'begonia-lite' ); ?> <div class="read-more--arrow"></div></a>
-					</div>
-					<?php
-				}
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . __( 'Pages:', 'begonia-lite' ),
-					'after'  => '</div>',
-				) ); ?>
+				<div class="c-hero_read_more">
+					<a href="<?php the_permalink(); ?>"><?php _e( 'Read More', 'begonia-lite' ); ?>
+						<div class="read-more--arrow"></div>
+					</a>
+				</div>
 			</div><!-- .entry-content -->
 		</div>
 	</header><!-- .entry-header -->
--- template-parts/content-no_top.php
+++ template-parts/content-no_top.php
@@ -9,47 +9,46 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class($class='c-card'); ?>>
-    <header class="entry-header">
+<article id="post-<?php the_ID(); ?>" <?php post_class( $class = 'c-card' ); ?>>
+	<header class="entry-header">
+
 		<?php if ( has_post_thumbnail() ) : ?>
-            <div class="entry-thumbnail">
-	            <a class="article__image  article__link" href="<?php the_permalink(); ?>">
+
+			<div class="entry-thumbnail">
+				<a class="article__image  article__link" href="<?php the_permalink(); ?>">
 					<?php the_post_thumbnail(); ?>
-	            </a><!-- .article__image -->
-            </div>
+				</a><!-- .article__image -->
+			</div>
+
 		<?php endif;
 		if ( 'post' === get_post_type() ) : ?>
+
 			<div class="entry-meta">
+
 				<?php begonia_lite_posted_on(); ?>
+
 			</div><!-- .entry-meta -->
+
 			<?php
 		endif;
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-		 ?>
-    </header><!-- .entry-header -->
+		endif; ?>
 
-    <div class="entry-content">
-		<?php
-		global $post;
-		// Check the content for the more text
-		$has_more = strpos( $post->post_content, '<!--more' );
-		if ( $has_more ) {
-			the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'begonia-lite' ) );
-		} else {
-			the_excerpt(); ?>
-            <div class="hero_read_more">
-                <a href="<?php the_permalink(); ?>"><?php _e( 'Read More', 'begonia-lite' ); ?><div class="read-more--arrow"></div></a>
-            </div>
-			<?php
-		}
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'begonia-lite' ),
-			'after'  => '</div>',
-		) ); ?>
-    </div><!-- .entry-content -->
+	</header><!-- .entry-header -->
 
+	<div class="entry-content">
+		<a href="<?php the_permalink(); ?>">
+
+			<?php the_excerpt(); ?>
+
+		</a>
+		<div class="hero_read_more">
+			<a href="<?php the_permalink(); ?>"><?php _e( 'Read More', 'begonia-lite' ); ?>
+				<div class="read-more--arrow"></div>
+			</a>
+		</div>
+	</div><!-- .entry-content -->
 </article><!-- #post-## -->
