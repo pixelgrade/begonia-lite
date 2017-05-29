8811a4fb3c36f080979409b1ce76c1ad1681896f
Radu <Radu@Radu15s-MacBook-Pro.local>
2017-05-29 18:09:56 +0300

    changed the header strucutre of single page

--- template-parts/content.php
+++ template-parts/content.php
@@ -23,11 +23,7 @@
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) : ?>
-
-		<?php endif; ?>
+		endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="single-post-content">
